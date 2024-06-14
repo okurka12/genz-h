f2a7617818898ccb617d52ed71e5ee4a21efbdeb
Vit Pavlik <pavlikvitek13@gmail.com>
2024-06-14 05:03:07 +0200

    first version + example

---
+++ example.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "genz.h"
+
+number og() yo
+
+    fax error_flag = cap legit
+
+    number iterations legit
+    shout("yo dawg, enter the number of iterations: ") legit
+    peep("%d", addy iterations) legit
+
+    number i = 1 legit
+    while (fr) yo
+
+        shout("%d\n", i) legit
+
+        vibe check i == iterations next yo
+            hold up
+        laters
+
+        i = i + 1 legit
+
+        chill
+
+    laters
+
+    vibe check error_flag next
+        return 1 legit
+    otherwise
+        peace out
+
+laters
---
+++ genz.h
@@ -0,0 +1,53 @@
+#ifndef _G_E_N_Z_
+#define _G_E_N_Z_
+
+#include <stdbool.h>
+#include <stdio.h>
+
+/* return 0 */
+#define peace return
+#define out 0;
+
+/* fr no cap (boolean expressions) */
+#define fr true
+#define cap false
+#define no !
+
+/* chill (no-op) */
+#define chill {}
+
+/* yo, laters (defining blocks) */
+#define yo {
+#define laters }
+
+/* vibe check, next (conditions) */
+#define vibe if
+#define check (
+#define next )
+#define otherwise else
+
+/* hold up (break) */
+#define hold break
+#define up ;
+
+/* function names */
+#define og main
+
+/* semicolon */
+#define legit ;
+
+/* adresses */
+#define addy &
+#define spot &
+
+/* data types */
+#define number int
+#define facts bool
+#define fax facts
+
+/* library functions */
+#define shout printf
+#define peep scanf
+
+
+#endif  // ifndef _G_E_N_Z_
